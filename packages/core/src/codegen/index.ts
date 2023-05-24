cdeba9a9b0c7ef4f8a8b99eeb5fb303ed6a186ba
SyMind <dacongsama@live.com>
2023-05-24 09:23:34 +0800

    feat: emit message

--- packages/core/src/codegen/index.ts
+++ packages/core/src/codegen/index.ts
@@ -33,6 +33,7 @@ export class CodeGenerator {
   emitSyntax(node: ast.Syntax): void {
     this.writer.writeRaw(`syntax = '${node.syntax}'`)
     this.writer.writeNewline()
+    this.writer.writeNewline()
   }
 
   emitImport(node: ast.Import): void {
@@ -65,7 +66,51 @@ export class CodeGenerator {
   }
 
   emitMessage(node: ast.Message): void {
+    this.writer.writeRaw(`message ${node.keyword.text} {`)
+    this.writer.writeNewline()
+    this.writer.increaseIndent()
 
+    this.emitMessageBody(node.messageBody)
+
+    this.writer.decreaseIndent()
+    this.writer.writeRaw('}')
+    this.writer.writeNewline()
+    this.writer.writeNewline()
+  }
+
+  emitMessageBody(node: ast.MessageBody): void {
+    for (const statement of node.statements) {
+      this.emitMessageBodyStatement(statement)
+    }
+  }
+
+  emitMessageBodyStatement(node: ast.MessageBodyStatement): void {
+    switch (node.type) {
+      case 'field':
+        return this.emitField(node)
+      case 'malformed-field':
+        return this.emitMalformedField(node)
+      case 'enum':
+        return this.emitEnum(node)
+      case 'message':
+        return this.emitMessage(node)
+      case 'extend':
+        return this.emitExtend(node)
+      case 'extensions':
+        return this.emitExtensions(node)
+      case 'group':
+        return this.emitGroup(node)
+      case 'option':
+        return this.emitOption(node)
+      case 'oneof':
+        return this.emitOneof(node)
+      case 'map-field':
+        return this.emitMapField(node)
+      case 'reserved':
+        return this.emitReserved(node)
+      case 'empty':
+        return this.emitEmpty(node)
+    }
   }
 
   emitEnum(node: ast.Enum): void {
@@ -77,6 +122,34 @@ export class CodeGenerator {
   }
 
   emitService(node: ast.Service): void {
+
+  }
+
+  emitField(node: ast.Field): void {
+
+  }
+
+  emitMalformedField(node: ast.MalformedField): void {
+
+  }
+
+  emitExtensions(node: ast.Extensions): void {
+
+  }
+
+  emitGroup(node: ast.Group): void {
+
+  }
+
+  emitOneof(node: ast.Oneof): void {
+
+  }
+
+  emitMapField(node: ast.MapField): void {
+
+  }
+
+  emitReserved(node: ast.Reserved): void {
 
   }
 }
